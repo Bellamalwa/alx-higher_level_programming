f793f0733eccfd76aa5181021f3effbaab28f5fa
Bellamalwa <duchybella@gmail.com>
2023-09-29 06:39:29 -0700

    bugfix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,11 +7,24 @@ import requests
 import sys
 
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: ./10-my_github.py <username> <token>")
+        sys.exit(1)
 
-    r = requests.get('https://api.github.com/user',
-                     auth=(sys.argv[1], sys.argv[2]))
-    json = r.json()
-    try:
-        print(json['id'])
-    except:
+    username, token = sys.argv[1], sys.argv[2]
+
+    # Make the request with Basic Authentication using your token
+    response = requests.get('https://api.github.com/user', auth=(username, token))
+
+    if response.status_code == 200:
+        try:
+            user_data = response.json()
+            user_id = user_data.get('id')
+            if user_id is not None:
+                print(user_id)
+            else:
+                print("None")
+        except ValueError:
+            print("Invalid JSON response")
+    else:
         print("None")
